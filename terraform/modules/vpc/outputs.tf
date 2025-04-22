b0f196469d2606493425419e2cb7380f7c014231
Dmitry Akshevsky <dmitry@akshevsky.ru>
2025-04-22 20:14:32 +0300

    chore(terraform): delete old vpc module

--- terraform/modules/vpc/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "network_id"  { value = yandex_vpc_network.network.id }
-output "subnet_ids"  {
-  value = { for k, v in yandex_vpc_subnet.subnet : k => v.id }
-}
-
-output "sg_id" {
-  description = "ID созданных security‑groups"
-  value = yandex_vpc_security_group.sg.id 
-}
